942dada6544416e871d3080a7196c4a6fbdf1cf0
txsc <spactial@163.com>
2020-03-05 22:24:22 +0800

    加了2句

--- src/com/atguigu/wugong/Dugujiujian.java
+++ src/com/atguigu/wugong/Dugujiujian.java
@@ -5,6 +5,8 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("沧海一声笑");
 		System.out.println("滔滔两岸潮");
+		System.out.println("无忧无虑看今朝");
+		System.out.println("沧海笑！");
 	}
 
 }
