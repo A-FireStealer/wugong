378d09716a9b90569ad978239cc6852c7bb2416d
A-FireStealer <61811336+A-FireStealer@users.noreply.github.com>
2020-03-14 19:13:14 +0800

    今日日志:更改项目整合,将14日所报的7处红色等级bug,进行整合并记录,其中涉及到后台数据的4处NotFondeException,进行了更正,另外3处后台,数据错乱和1处空指针异常,需要商榷后处理.计划,2日内,完成.
    14日工作总结日志

--- src/com/atguigu/wugong/Dugujiujian.java
+++ src/com/atguigu/wugong/Dugujiujian.java
@@ -14,3 +14,76 @@ public class Dugujiujian {
 	}
 
 }
+package demo.collection;
+
+import java.util.*;
+
+import demo.bean.Book;
+
+/**
+ * 此类用于演示使用Collection集合的遍历
+ * @author liyuting
+ * 
+ * 遍历：按一定的路径，依次将元素取出来使用
+ *
+ * 遍历方式：
+ * 1、使用iterator迭代器
+ * 2、jdk5.0出现了增强for循环，可以代替iterator迭代器
+ * 特点：增强for就是简化版的iterator，本质一样
+ * 
+ * 只能用于遍历集合或数组
+ * 语法：
+ * for(元素类型 元素名 :  集合名或数组名){
+ * 		访问元素
+ * }
+ * 
+ * 
+ */
+public class TestCollection3 {
+	@SuppressWarnings({"rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		//1.创建集合对象
+		Collection col = new ArrayList();
+		
+		//2.添加元素
+		col.add(new Book("三国演义","罗贯中"));
+		col.add(new Book("小李飞刀","古龙"));
+		col.add(new Book("红楼梦","曹雪芹"));
+		col.add(new Book("十万个为什么","佚名"));
+		
+		
+		//3.遍历
+		iterCollection1(col);
+	
+	}
+	/**
+	 * 遍历方式一：使用iterator
+	 * @param col
+	 */
+	@SuppressWarnings("rawtypes")
+	public static void iterCollection1(Collection col){
+		//①获取该集合的迭代器
+		Iterator iterator = col.iterator();
+		//②进行迭代
+		while(iterator.hasNext()){
+			Object next = iterator.next();
+//			col.remove(new Book("三国演义","罗贯中"));
+			
+			iterator.remove();
+			
+			System.out.println(next);
+		}
+		
+	}
+	/**
+	 * 遍历方式二：使用增强for
+	 * @param col
+	 */
+	public static void iterCollection2(Collection col){
+		
+		for(Object o: col){
+			System.out.println(o);
+		}
+	}
+
+}
